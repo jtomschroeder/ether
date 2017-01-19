6a69540d4e624298918beaff0f6d37008e860a1f
Tom Schroeder <j.tom.schroeder@gmail.com>
2017-01-18 19:25:50 -0600

    Cleanup module docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,32 @@
 
-//! This crate provides a library for parsing and manipulating network data, packet captures.
-//!
-//! # Usage
-//!
-//! Add this to your `Cargo.toml`:
-//!
-//! ```toml
-//! [dependencies]
-//! ether = "0.0"
-//! ```
-//!
-//! and this to *root* of your crate:
-//!
-//! ```rust
-//! extern crate ether;
-//! ```
-//!
-//! Here's a simple example that prints all packets received on interface `en0`:
-//!
-//! ```rust,no_run
-//! extern crate futures;
-//! extern crate ether;
-//!
-//! use futures::stream::Stream;
-//! use ether::tap;
-//!
-//! fn main() {
-//!     let mut tap = tap::Tap::new("en0").unwrap();
-//!     for packet in tap.stream().wait().filter_map(|p| p.ok()) {
-//!         println!("{:?}", packet);
-//!     }
-//! }
-//! ```
-//!
+/*!
+This crate provides a library for parsing and manipulating network data, packet captures.
+
+# Usage
+
+Add `ether` to the dependencies in your `Cargo.toml` and the following to *root* of your crate:
+
+```rust
+extern crate ether;
+```
+
+Here's a simple example that prints all packets received on interface `en0`:
+
+```rust,no_run
+extern crate futures;
+extern crate ether;
+
+use futures::stream::Stream;
+use ether::tap;
+
+fn main() {
+    let mut tap = tap::Tap::new("en0").unwrap();
+    for packet in tap.stream().wait().filter_map(|p| p.ok()) {
+        println!("{:?}", packet);
+    }
+}
+```
+*/
 
 extern crate num;
 extern crate libc;
--- src/pcap/mod.rs
+++ src/pcap/mod.rs
@@ -1,36 +1,38 @@
 
-//! Parse packet captures as a 'header' and list of 'records'
-//!
-//! # Usage:
-//! ```rust,no_run
-//! extern crate ether;
-//! use ether::pcap;
-//!
-//! fn run() -> std::io::Result<()> {
-//!     use std::fs::File;
-//!     use std::io::Read;
-//!
-//!     let mut buffer = Vec::new();
-//!     let mut file = try!(File::open("capture.pcap"));
-//!     try!(file.read_to_end(&mut buffer));
-//!
-//!     let pcap = pcap::PacketCapture::new(buffer);
-//!     match pcap.parse().unwrap() {
-//!        (pcap::Header { network: pcap::Link::Ethernet, .. }, records) => {
-//!            for pcap::Record { payload, .. } in records {
-//!                println!("ethernet: {:?}", payload);
-//!            }
-//!        }
-//!        _ => {}
-//!     }
-//!
-//!     Ok(())
-//! }
-//!
-//! fn main() {
-//!    run().unwrap();
-//! }
-//! ```
+/*!
+Parse packet captures as a 'header' and list of 'records'
+
+# Usage:
+```rust,no_run
+extern crate ether;
+use ether::pcap;
+
+fn run() -> std::io::Result<()> {
+    use std::fs::File;
+    use std::io::Read;
+
+    let mut buffer = Vec::new();
+    let mut file = try!(File::open("capture.pcap"));
+    try!(file.read_to_end(&mut buffer));
+
+    let pcap = pcap::PacketCapture::new(buffer);
+    match pcap.parse().unwrap() {
+       (pcap::Header { network: pcap::Link::Ethernet, .. }, records) => {
+           for pcap::Record { payload, .. } in records {
+               println!("ethernet: {:?}", payload);
+           }
+       }
+       _ => {}
+    }
+
+    Ok(())
+}
+
+fn main() {
+   run().unwrap();
+}
+```
+*/
 
 mod pcap;
 pub use self::pcap::*;
