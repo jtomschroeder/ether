995ccb7587662a4ae823ac408b6ec59610fadc29
Tom Schroeder <j.tom.schroeder@gmail.com>
2017-01-29 19:12:19 -0600

    Improve RawSocket impl.

--- src/bin/ping.rs
+++ src/bin/ping.rs
@@ -5,6 +5,7 @@ extern crate itertools;
 
 use std::io;
 use std::mem;
+use std::net;
 
 use ether::packet::network::{ipv4, icmp};
 
@@ -22,6 +23,8 @@ extern crate futures;
 extern crate nix;
 
 use std::os::unix::io::RawFd;
+use std::ops;
+
 use nix::fcntl::{fcntl, FcntlArg, O_NONBLOCK};
 
 use mio::Evented;
@@ -30,18 +33,47 @@ use tokio_core::reactor::{Core, Handle, PollEvented};
 use futures::{Stream, Poll, Async};
 
 #[derive(Debug)]
-pub struct EventedFile {
-    fd: RawFd,
+pub struct RawSocket(RawFd);
+
+impl RawSocket {
+    fn new() -> io::Result<Self> {
+        use libc::*;
+        match unsafe { socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP) } {
+            -1 => Err(io::Error::last_os_error()),
+            s => Ok(RawSocket(s)),
+        }
+    }
+
+    fn raw(&self) -> RawFd {
+        self.0
+    }
+
+    fn recv_from(&self, buffer: &mut [u8], addr: net::Ipv4Addr) -> io::Result<usize> {
+        let whereto = destination(addr);
+        let addrlen = 16u32;
+
+        unsafe {
+            match libc::recvfrom(self.raw(),
+                                 mem::transmute(buffer.as_ptr()),
+                                 buffer.len(),
+                                 0,
+                                 mem::transmute(&whereto),
+                                 mem::transmute(&addrlen)) {
+                -1 => Err(io::Error::last_os_error()),
+                otherwise => Ok(otherwise as usize),
+            }
+        }
+    }
 }
 
-impl Evented for EventedFile {
+impl<'a> Evented for &'a RawSocket {
     fn register(&self,
                 poll: &mio::Poll,
                 token: mio::Token,
                 interest: mio::Ready,
                 opts: mio::PollOpt)
                 -> io::Result<()> {
-        EventedFd(&self.fd).register(poll, token, interest, opts)
+        EventedFd(&self.raw()).register(poll, token, interest, opts)
     }
 
     fn reregister(&self,
@@ -50,62 +82,51 @@ impl Evented for EventedFile {
                   interest: mio::Ready,
                   opts: mio::PollOpt)
                   -> io::Result<()> {
-        EventedFd(&self.fd).reregister(poll, token, interest, opts)
+        EventedFd(&self.raw()).reregister(poll, token, interest, opts)
     }
 
     fn deregister(&self, poll: &mio::Poll) -> io::Result<()> {
-        EventedFd(&self.fd).deregister(poll)
+        EventedFd(&self.raw()).deregister(poll)
     }
 }
 
-pub struct RawSocketStream {
-    fd: RawFd,
-    evented: PollEvented<EventedFile>,
+pub struct RawSocketStream<'a> {
+    socket: &'a RawSocket,
+    evented: PollEvented<&'a RawSocket>,
 }
 
-impl RawSocketStream {
-    pub fn new(evf: EventedFile, handle: &Handle) -> io::Result<Self> {
+impl<'a> RawSocketStream<'a> {
+    pub fn new<'s>(socket: &'s RawSocket, handle: &Handle) -> io::Result<RawSocketStream<'s>> {
         Ok(RawSocketStream {
-            fd: evf.fd,
-            evented: try!(PollEvented::new(evf, &handle)),
+            socket: socket,
+            evented: try!(PollEvented::new(socket, &handle)),
         })
     }
 }
 
-fn destination() -> libc::sockaddr_in {
-    let mut whereto: libc::sockaddr_in = unsafe { mem::zeroed() }; // who to ping
+fn destination(addr: net::Ipv4Addr) -> libc::sockaddr_in {
+    // let addr = net::Ipv4Addr::new(216, 58, 216, 238); // google.com
+
+    let mut whereto: libc::sockaddr_in = unsafe { mem::zeroed() };
     whereto.sin_family = libc::AF_INET as u8;
-    whereto.sin_len = 16; // sizeof sockaddr_in
-    whereto.sin_addr.s_addr = 0xEED83AD8; // google.com @ 216.58.216.238 (little endian)
+    whereto.sin_len = 16; // sizeof(sockaddr_in)
+
+    let addr: u32 = addr.into();
+    whereto.sin_addr.s_addr = addr.to_be();
+
     whereto
 }
 
-impl Stream for RawSocketStream {
+impl<'a> Stream for RawSocketStream<'a> {
     type Item = Vec<u8>;
     type Error = io::Error;
 
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
         Ok(match self.evented.poll_read() {
             Async::Ready(_) => {
-                self.evented.need_read();
-
-                let buffer = vec![0u8; IP_MAXPACKET];
-                let addrlen = 16u32;
-
-                let whereto = destination();
-
-                let recvd = unsafe {
-                    match libc::recvfrom(self.fd,
-                                         mem::transmute(buffer.as_ptr()),
-                                         buffer.len(),
-                                         0,
-                                         mem::transmute(&whereto),
-                                         mem::transmute(&addrlen)) {
-                        -1 => return Err(io::Error::last_os_error()),
-                        otherwise => otherwise as usize,
-                    }
-                };
-
+                let mut buffer = vec![0u8; IP_MAXPACKET];
+                let addr = net::Ipv4Addr::new(216, 58, 216, 238);
+                let recvd = try!(self.socket.recv_from(&mut buffer, addr));
                 Async::Ready(Some(buffer[..recvd].to_vec()))
             }
             Async::NotReady => Async::NotReady,
@@ -121,22 +142,18 @@ fn run() -> io::Result<()> {
         .sequence(0)
         .build("TEST".as_bytes());
 
+    let addr = net::Ipv4Addr::new(216, 58, 216, 238); // google.com
+
     let s = unsafe {
         use std::mem;
         use libc::*;
 
-        let s = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
-        if s < 0 {
-            return Err(io::Error::last_os_error());
-        }
+        let s = try!(RawSocket::new());
 
-        let whereto = destination();
-
-        // let hold = IP_MAXPACKET + 128;
-        // setsockopt(s, SOL_SOCKET, SO_RCVBUF, mem::transmute(&hold), 4);
+        let whereto = destination(addr);
 
         // Send echo request
-        let i = sendto(s,
+        let i = sendto(s.raw(),
                        mem::transmute(packet.as_ptr()),
                        packet.len(),
                        0,
@@ -152,9 +169,9 @@ fn run() -> io::Result<()> {
     let mut core = try!(Core::new());
     let handle = core.handle();
 
-    try!(fcntl(s, FcntlArg::F_SETFL(O_NONBLOCK)));
+    try!(fcntl(s.raw(), FcntlArg::F_SETFL(O_NONBLOCK)));
 
-    let stream = try!(RawSocketStream::new(EventedFile { fd: s }, &handle));
+    let stream = try!(RawSocketStream::new(&s, &handle));
 
     try!(core.run(stream.take(1).for_each(|buffer| {
         println!("{:?}", buffer);
@@ -171,7 +188,7 @@ fn run() -> io::Result<()> {
     })));
 
     unsafe {
-        libc::close(s);
+        libc::close(s.raw());
     }
 
     Ok(())
